aa7daeb1dbbd6cb41a28be9cbbc0a31a8cb22cac
sreevidya <sreevidyagu98@gmail.com>
2025-09-18 12:23:30 +0530

    Updated src/components/App.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,45 @@ import logo from "./components/images/logo.webp";
 function App() {
   return (
     <HashRouter>
-      <div className="header">
-        <img src={logo} alt="logo" />
-        <nav>
-          <Link to="/">Home</Link> <Link to="/about">About</Link>{" "}
-          <Link to="/socialimpact">Socialimpact</Link>{" "}
-          <Link to="/contact">Contact</Link>
-        </nav>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <div class="logo">
+              <img src={logo} class="img-fluid" alt="logo" />
+            </div>
+          </div>
+          <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
+              <input id="main-menu-state" type="checkbox" />
+              <label class="main-menu-btn" for="main-menu-state">
+                <span class="main-menu-btn-icon"></span> Toggle main menu
+                visibility
+              </label>
+              <ul id="main-menu" class="sm sm-blue">
+                <li>
+                  <a href="/about" class="basic-3">
+                    About
+                  </a>
+                </li>
+                {/* <li><a href="products.html" class="basic-3">Products</a>
+						<ul>
+							<li><a class="dropdown-item" href="products.html">Incense Sticks</a></li>
+						</ul>
+					</li> */}
+                <li>
+                  <a href="/socialimpact" class="basic-3">
+                    Social Impact
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact" class="basic-3">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
 
       <div>
