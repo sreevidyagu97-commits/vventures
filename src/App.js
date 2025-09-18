d74b2e8808de99b404f4feb88848e7710989565b
sreevidya <sreevidyagu98@gmail.com>
2025-09-18 12:27:58 +0530

    Updated src/ App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
