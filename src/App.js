568532000e69d41c797f7bac88fa879e67a0ad10
sreevidya <sreevidyagu98@gmail.com>
2025-09-18 12:41:49 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -48,12 +48,12 @@ function App() {
       </div>
 
       <div>
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/socialimpact" element={<Socialimpact />} />
-        </Routes>
+        </Routes> */}
       </div>
 
       <Footer />
