8a663582a5ae3009521bb995e84993e27e2d97a0
sreevidya <sreevidyagu98@gmail.com>
2025-09-18 11:30:26 +0530

    Updated routing with HashRouter and CSS changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -33,24 +8,18 @@ import Socialimpact from "./components/Socialimpact";
 import Footer from "./components/Footer";
 import logo from "./components/images/logo.webp";
 
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
-
 function App() {
   return (
-    <Router>
+    <>
       <div className="header">
         <img src={logo} alt="logo" />
         <nav>
-          <Link to="/">Home</Link> {""}
-          <Link to="/about">About</Link>{" "}
+          <Link to="/">Home</Link> <Link to="/about">About</Link>{" "}
           <Link to="/socialimpact">Socialimpact</Link>{" "}
           <Link to="/contact">Contact</Link>
         </nav>
       </div>
+
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -61,7 +30,7 @@ function App() {
       </div>
 
       <Footer />
-    </Router>
+    </>
   );
 }
 
